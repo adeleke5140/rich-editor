9c32b885f443627d8d2fdff6be3054aa22d7e95e
Kenny <adelekekehinde06@gmail.com>
2024-01-10 14:06:47 +0100

    style: add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text-color': 'rgba(255, 255, 255, 0.443)',
+        'hover-color': 'rgba(255, 255, 255, 0.055)',
+      },
+      boxShadow: {
+        dark: 'rgba(15, 15, 15, 0.1) 0px 0px 0px 1px, rgba(15, 15, 15, 0.2) 0px 3px 6px, rgba(15, 15, 15, 0.4) 0px 9px 24px',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
