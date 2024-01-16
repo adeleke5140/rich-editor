0d4c44ea835be370a47ea03043ce9d4671a66593
Kenny <adelekekehinde06@gmail.com>
2024-01-16 14:22:42 +0100

    feat: add utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -23,3 +23,17 @@ export function getUrlFromString(str: string) {
     return null;
   }
 }
+
+export function updateScrollView(container: HTMLElement, item: HTMLElement) {
+  const containerHeight = container.offsetHeight;
+  const itemHeight = item ? item.offsetHeight : 0;
+
+  const top = item.offsetTop;
+  const bottom = top + itemHeight;
+
+  if (top < container.scrollTop) {
+    container.scrollTop -= container.scrollTop - top + 10;
+  } else if (bottom > containerHeight + container.scrollTop) {
+    container.scrollTop += bottom - containerHeight - container.scrollTop + 10;
+  }
+}
