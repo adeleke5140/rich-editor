fe2b4e0fdfb57a61511696b6d3baf38097d152b2
Kenny <adelekekehinde06@gmail.com>
2024-01-16 14:20:46 +0100

    feat: add debug mode

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,37 @@ import { TipTapEditorExtensions } from './components/editor/extensions';
 import { TipTapEditorProps } from './components/editor/props';
 import applyDevTools from 'prosemirror-dev-tools';
 
+//show prosemirror dev tools
+const DEV_MODE = import.meta.env.DEV;
+
+const searchParams = new URLSearchParams(window.location.search);
+const debugParam = searchParams.get('debug');
+
+//show devtools in dev environment or in dev mode
+const DEBUG = debugParam === 'true' ? debugParam : DEV_MODE;
+
+const fonts = [
+  {
+    name: 'default',
+    fontFamily: 'Atkinson Hyperlegible, sans-serif',
+  },
+  {
+    name: 'serif',
+    fontFamily: 'Spectral, serif',
+  },
+  {
+    name: 'mono',
+    fontFamily: 'Ubuntu Mono, monospace',
+  },
+];
+
+const defaultFont = fonts[0].fontFamily;
+
 function App() {
   const [content, setContent] = useState<ReturnType<Editor['getJSON']> | null>(
     null
   );
+  const [fontFamily, setFontFamily] = useState(defaultFont);
 
   const updateContent = (editor: Editor) => {
     const json = editor.getJSON();
@@ -20,7 +47,7 @@ function App() {
   };
   const editor = useEditor({
     onCreate({ editor }) {
-      applyDevTools(editor.view);
+      DEBUG ? applyDevTools(editor.view) : null;
     },
     extensions: [...TipTapEditorExtensions],
     content,
@@ -30,14 +57,40 @@ function App() {
     },
   });
   return (
-    <div
-      onClick={() => {
-        editor?.chain().focus().run();
-      }}
-      className="p-2 relative max-w-2xl mx-auto min-h-screen"
-    >
-      {editor && <EditorBubbleMenu editor={editor} />}
-      <EditorContent editor={editor} className="h-full" />
+    <div className="max-w-screen-lg mx-auto">
+      <div className="ml-auto text-white m-4 space-y-2">
+        <h1 className="font-bold text-lg">Font</h1>
+        <div className="flex gap-2">
+          {fonts.map((font) => (
+            <button
+              key={font.fontFamily}
+              onClick={() => {
+                setFontFamily(font.fontFamily);
+              }}
+              className={`border border-stone-400 p-2 rounded-md text-sm capitalize ${
+                font.fontFamily === fontFamily
+                  ? 'bg-stone-700 text-white font-semibold'
+                  : ''
+              }`}
+            >
+              {font.name}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div
+        onClick={() => {
+          editor?.chain().focus().run();
+        }}
+        className="relative mt-8 p-4 min-h-[500px] w-full max-w-screen-lg border-stone-200 sm:rounded-lg sm:border sm:shadow-lg mx-auto"
+        style={{
+          fontFamily,
+        }}
+      >
+        {editor && <EditorBubbleMenu editor={editor} />}
+        <EditorContent editor={editor} className="h-full" />
+      </div>
+      <footer className="text-stone-200 mt-2">© kepler</footer>
     </div>
   );
 }
