fc80c4e509619472952ebbf3e4d18c707e5ac5b2
Kenny <adelekekehinde06@gmail.com>
2024-01-16 14:21:17 +0100

    feat: major updates

--- src/components/CommandList.tsx
+++ src/components/CommandList.tsx
@@ -1,16 +1,22 @@
-import { useState, useRef, useCallback, useEffect } from 'react';
+import {
+  useState,
+  useRef,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+} from 'react';
 import { CommandItemProps } from './Command';
+import { updateScrollView } from '../lib/utils';
 
 export const CommandList = ({
   items,
   command,
 }: {
   items: CommandItemProps[];
-  command: (item: CommandItemProps) => void;
+  command: any;
 }) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const commandListContainer = useRef<HTMLDivElement>(null);
-  const selectedButtonRef = useRef<HTMLButtonElement>(null);
 
   const selectItem = useCallback(
     (index: number) => {
@@ -24,18 +30,18 @@ export const CommandList = ({
 
   useEffect(() => {
     const navigationKeys = ['ArrowUp', 'ArrowDown', 'Enter'];
-    const onKeyDown = (event: KeyboardEvent) => {
-      if (navigationKeys.includes(event.key)) {
-        event.preventDefault();
-        if (event.key === 'ArrowUp') {
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (navigationKeys.includes(e.key)) {
+        e.preventDefault();
+        if (e.key === 'ArrowUp') {
           setSelectedIndex((selectedIndex + items.length - 1) % items.length);
           return true;
         }
-        if (event.key === 'ArrowDown') {
+        if (e.key === 'ArrowDown') {
           setSelectedIndex((selectedIndex + 1) % items.length);
           return true;
         }
-        if (event.key === 'Enter') {
+        if (e.key === 'Enter') {
           selectItem(selectedIndex);
           return true;
         }
@@ -46,55 +52,47 @@ export const CommandList = ({
     return () => {
       document.removeEventListener('keydown', onKeyDown);
     };
-  }, [items, selectItem, selectedIndex, setSelectedIndex]);
+  }, [items, selectedIndex, setSelectedIndex, selectItem]);
 
   useEffect(() => {
     setSelectedIndex(0);
   }, [items]);
 
-  useEffect(() => {
-    const container = commandListContainer.current;
-    const selectedButton = selectedButtonRef.current;
+  useLayoutEffect(() => {
+    const container = commandListContainer?.current;
 
-    if (container && selectedButton) {
-      container.scrollTop = selectedButton.offsetTop - container.offsetTop;
+    const item = container?.children[selectedIndex] as HTMLElement;
 
-      selectedButton.focus();
-    }
-  }, [selectedIndex, items]);
+    if (item && container) updateScrollView(container, item);
+  }, [selectedIndex]);
 
   return items.length > 0 ? (
     <div
       ref={commandListContainer}
-      className="z-30 flex flex-col shadow-dark gap-2 max-h-[330px] bg-[rgb(37_37_37)] w-72 overflow-y-auto scroll-smooth rounded-md px-1 py-1 transition-all"
+      id="slash-command"
+      className="z-30 flex h-auto border border-stone-700 flex-col  shadow-dark gap-2 max-h-[330px] bg-[rgb(37_37_37)] w-72 overflow-y-auto scroll-smooth rounded-md p-2 transition-all"
     >
-      <p className="p-2 text-text-color font-medium select-none">
-        Basic blocks
-      </p>
-      <div className="flex flex-col gap-2">
-        {items.map((item, index) => (
-          <button
-            key={item.title}
-            ref={index === selectedIndex ? selectedButtonRef : null}
-            className={`flex items-center hover:bg-hover-color space-x-2 w-full p-2 rounded-md hover:bg-gray-100 focus:outline-none ${
-              index === selectedIndex ? 'bg-hover-color' : ''
-            }`}
-            onClick={() => {
-              selectItem(index);
-            }}
-          >
-            <div className="flex h-10 w-10 items-center justify-center rounded-md border border-gray-200 bg-white">
-              {item.icon}
-            </div>
-            <div className="flex flex-col text-left">
-              <div className="text-sm font-medium text-white">{item.title}</div>
-              <div className="text-xs text-[rgb(127_127_127)]">
-                {item.description}
-              </div>
-            </div>
-          </button>
-        ))}
-      </div>
+      {items.map((item, index) => (
+        <button
+          key={item.title}
+          className={`flex items-center hover:bg-hover-color space-x-2 w-full px-2 py-1 rounded-md hover:bg-gray-100 focus:outline-none ${
+            index === selectedIndex ? 'bg-hover-color' : ''
+          }`}
+          onClick={() => {
+            selectItem(index);
+          }}
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-md border border-gray-200 bg-white">
+            {item.icon}
+          </div>
+          <div className="flex flex-col text-left">
+            <p className="text-sm font-medium text-white">{item.title}</p>
+            <p className="text-xs text-[rgb(127_127_127)]">
+              {item.description}
+            </p>
+          </div>
+        </button>
+      ))}
     </div>
   ) : null;
 };
